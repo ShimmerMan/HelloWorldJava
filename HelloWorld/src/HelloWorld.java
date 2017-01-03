75969c19713a5c0e6901db8a01c0741ca7ba5900
ShimmerMan <jeremy.t.larsen@gmail.com>
2017-01-03 22:37:42 +1000

    And one more commit, adding an irrelevant line to the message.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!\n" + "Hello to you too!");
+		System.out.println("Hello World!\n" + "Hello to you too!\n" + "The cake really was a lie...");
 	}
 
 }
